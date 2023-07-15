9f5dc1a8dfd5812c0085248827a428f808868a52
Eduardo Citelli <eduardo.citelli@redjar.com.ar>
2023-07-15 10:29:14 -0300

    Add project files.

---
+++ Ejercicio1_Asbtraccion/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio1_Asbtraccion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
